d7b5b675e0a6beede68a3e3d1d5807d2075d134b
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-04-02 00:04:04 +0200

    List all implementation

--- Online-Store-Platform/src/main/java/com/SWE/OnlineStorePlatform/services/UsersController.java
+++ Online-Store-Platform/src/main/java/com/SWE/OnlineStorePlatform/services/UsersController.java
@@ -1,15 +1,25 @@
 package com.SWE.OnlineStorePlatform.services;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import com.SWE.OnlineStorePlatform.models.User;
+
 
 @RestController
 public class UsersController {
 
+	@Autowired
+	private UserService service;
 	@RequestMapping("/get-user-list")
 	public String getRegisteredUsers() {
+		List<User> userList = service.listAll();
 		return null;
 	}
-	@RequestMapping("/add-user")
-	public Boolean registerUser() {
+	@RequestMapping("/register")
+	public Boolean registerUser(HttpServletRequest request) {
 		return null;
 	}
 }
