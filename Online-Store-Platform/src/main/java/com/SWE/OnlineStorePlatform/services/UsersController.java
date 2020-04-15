61a5a1fd93fcca076807134eb9f4f1a57d6c60a2
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-04-15 21:43:50 +0200

    token generator with XOR

--- Online-Store-Platform/src/main/java/com/SWE/OnlineStorePlatform/services/UsersController.java
+++ Online-Store-Platform/src/main/java/com/SWE/OnlineStorePlatform/services/UsersController.java
@@ -1,4 +1,5 @@
 package com.SWE.OnlineStorePlatform.services;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -14,22 +15,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import net.minidev.json.JSONObject;
+
 import com.SWE.OnlineStorePlatform.models.*;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
 
 @RestController
 public class UsersController {
 
 	@Autowired
 	private UserService service;
+
 	@RequestMapping("/get-user-list")
 	public List<User> getRegisteredUsers() {
 		List<User> userList = service.listAll();
 		return userList;
 	}
-	
-	
+
 	@RequestMapping("/register")
 	public Boolean registerUser(HttpServletRequest request) {
 		String emailPattern = "^([a-zA-Z0-9_\\.]+)@[a-zA-Z_]+?\\.[a-zA-Z]{2,4}$";
@@ -41,13 +43,13 @@ public class UsersController {
 		int type = Integer.parseInt(typeString);
 		User user;
 		boolean isValid = username.matches(usernamePattern) || email.matches(emailPattern);
-		
-		switch(type) {
-			case 0:{
+
+		switch (type) {
+			case 0: {
 				user = new Buyer(email, username, pass, Type.BUYER);
 				break;
 			}
-			case 1:{
+			case 1: {
 				user = new Owner(email, username, pass, Type.OWNER);
 				break;
 			}
@@ -55,33 +57,43 @@ public class UsersController {
 				user = null;
 				break;
 		}
-		
-		if(!isValid || user == null)
-			{return false;}
-		else {
+
+		if (!isValid || user == null) {
+			return false;
+		} else {
 			service.save(user);
-			{return true;}
+			{
+				return true;
+			}
 		}
-			
+
 	}
 
 	@RequestMapping("/login")
 	@ResponseBody
-	public ResponseEntity<User> login(HttpServletRequest request) {
+	public ResponseEntity<String> login(HttpServletRequest request) {
 		String email_username = request.getParameter("email_username");
 		String pass = request.getParameter("password");
-		
+
 		User user = service.checkUserLogin(email_username);
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-Type", "application/json");
-	    ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.ok().headers(headers);
-	    
-	    if(user == null)
+		ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.ok().headers(headers);
+		if (user == null)
 			return responseBuilder.build();
-	    if(user.getPassword().equals(pass))
-			return new ResponseEntity<User>(user, HttpStatus.OK);
-	    return responseBuilder.build();
-	
+		if (user.getPassword().equals(pass))
+			return new ResponseEntity<String>("", HttpStatus.OK);
+		return responseBuilder.build();
+
+	}
+
+	private Token generateToken(String userID, String password) {
+		String token = userID + "-" + password;
+		int key = 512;
+		String generated = "";
+		for (int i = 0; i < (int) token.length(); ++i)
+			generated += (char) token.charAt(i) ^ key;
+		return new Token(generated, key);
 	}
 
 }
