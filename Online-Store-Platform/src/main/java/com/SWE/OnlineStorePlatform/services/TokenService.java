67be6a52e8526e1a51fe1849bd73a98e835bc780
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-04-15 22:09:20 +0200

    Created token service

---
+++ Online-Store-Platform/src/main/java/com/SWE/OnlineStorePlatform/services/TokenService.java
@@ -0,0 +1,31 @@
+package com.SWE.OnlineStorePlatform.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.SWE.OnlineStorePlatform.models.Token;
+import com.SWE.OnlineStorePlatform.repositories.TokenRepo;
+
+@Service
+@Transactional
+public class TokenService {
+    @Autowired
+    private TokenRepo repo;
+
+    public List<Token> listAll() {
+        return repo.findAll();
+    }
+
+    public void save(Token token) {
+        repo.save(token);
+    }
+
+    public Token get(String token) {
+        return repo.findByToken(token);
+    }
+
+    public void delete(String token) {
+        repo.deleteByToken(token);
+    }
+}
