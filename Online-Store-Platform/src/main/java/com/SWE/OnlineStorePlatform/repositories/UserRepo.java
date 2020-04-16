46d1306ddeb6c6a75a468a1fe7a478f86a1e32e2
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-04-16 16:02:39 +0200

    organized imports in user repo

--- Online-Store-Platform/src/main/java/com/SWE/OnlineStorePlatform/repositories/UserRepo.java
+++ Online-Store-Platform/src/main/java/com/SWE/OnlineStorePlatform/repositories/UserRepo.java
@@ -1,13 +1,9 @@
 package com.SWE.OnlineStorePlatform.repositories;
 
-import java.util.List;
-import java.util.Optional;
-
 import com.SWE.OnlineStorePlatform.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepo extends JpaRepository<User, Long> {// CrudRepository<User, Long>{//
